408f19c6d83eb49cb7cb50861faa204452d140ec
ce-bu <0801150+ce-bu@users.noreply.github.com>
2024-12-14 21:37:52 -0500

    eglot

--- emacs/use_eglot.el
+++ emacs/use_eglot.el
@@ -16,30 +16,31 @@
   :config
   (add-to-list 'eglot-stay-out-of 'flymake))
 
+
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '((rust-mode)
+                 . ("~/.cargo/bin/rust-analyzer"
+                    ))))
+
 (add-hook 'before-save-hook
           (lambda ()
 	    (when (eq major-mode 'rust-mode)
 	      (eglot-format))))
 
 
-;; (use-package eglot
-;;   :hook (
-;; ;         (haskell-mode . eglot-ensure)
-;;          (c++-mode . eglot-ensure)
-;;          (c-mode . eglot-ensure)
-;;          (rust-mode . eglot-ensure)
-;;          ))
+(use-package flycheck :config (global-flycheck-mode))
+(use-package company
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.5)
+  (company-tooltip-align-annotations t)
+  (company-tooltip-margin 2)
+  :config
+  (global-company-mode))
 
-;; (use-package flycheck :config (global-flycheck-mode))
-;; (use-package company
-;;   :custom
-;;   (company-minimum-prefix-length 1)
-;;   (company-idle-delay 0.5)
-;;   (company-tooltip-align-annotations t)
-;;   (company-tooltip-margin 2)
-;;   :config
-;;   (global-company-mode))
-
+(global-set-key "\M-a" 'eglot-code-actions)
 ;; (with-eval-after-load 'eglot
 ;;   (add-to-list 'eglot-server-programs
 ;;                '((c-mode c++-mode)
@@ -56,11 +57,4 @@
 ;; ;                    "--header-insertion=never"
 ;; ;                    "--header-insertion-decorators=0"
 ;; 		    ))))
-
-
-;; (with-eval-after-load 'eglot
-;;   (add-to-list 'eglot-server-programs
-;;                '((rust-mode)
-;;                  . ("~/.cargo/bin/rust-analyzer"
-;;                     ))))
 

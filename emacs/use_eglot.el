33ca33e6c98a394f3f78c40c5401ddc04591b928
ce-bu <0801150+ce-bu@users.noreply.github.com>
2024-12-14 21:54:16 -0500

    eglot

--- emacs/use_eglot.el
+++ emacs/use_eglot.el
@@ -41,20 +41,22 @@
   (global-company-mode))
 
 (global-set-key "\M-a" 'eglot-code-actions)
-;; (with-eval-after-load 'eglot
-;;   (add-to-list 'eglot-server-programs
-;;                '((c-mode c++-mode)
-;;                  . ("/Users/ubuser/opt/llvm-15.0.7/bin/clangd"
-;;                     "-j=8"
-;;                     "--log=error"
-;; 		    "--use-dirty-headers"
-;; ;                    "--malloc-trim"
-;;                     "--background-index"
-;; ;                    "--clang-tidy=0"
-;;  ;                   "--cross-file-rename"
-;;                     "--completion-style=detailed"
-;; ;                    "--pch-storage=memory"
-;; ;                    "--header-insertion=never"
-;; ;                    "--header-insertion-decorators=0"
-;; 		    ))))
+
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '((c-mode c++-mode)
+                 . ("/usr/bin/clangd"
+                    "-j=8"
+                    "--log=error"
+		    "--use-dirty-headers"
+;                    "--malloc-trim"
+                    "--background-index"
+;                    "--clang-tidy=0"
+ ;                   "--cross-file-rename"
+                    "--completion-style=detailed"
+;                    "--pch-storage=memory"
+;                    "--header-insertion=never"
+;                    "--header-insertion-decorators=0"
+		    ))))
 

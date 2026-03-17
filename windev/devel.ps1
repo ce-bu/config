1a9b962b07cfa9a01c769d12884065f3e92551af
ce-bu <60801150+ce-bu@users.noreply.github.com>
2026-03-17 10:25:57 -0400

    cleanup

--- windev/devel.ps1
+++ windev/devel.ps1
@@ -1,12 +1,13 @@
 # Assume the config is checked out under c:\Tools\Config
 # Windows shortcut
-# C:\Windows\System32\cmd.exe /k C:\Tools\Powershell\pwsh.exe -ExecutionPolicy Bypass -NoExit -Command " & { C:\Tools\Config\windev\devel.ps1 -ToolsDir C:\Tools } "
+# C:\Windows\System32\cmd.exe /k C:\Tools\Powershell\pwsh.exe -ExecutionPolicy Bypass -NoExit -Command " & { C:\Tools\Config\windev\devel.ps1 -ToolsDir C:\Tools -StartupDir C:\ } "
 
 # Terminal command 
-# C:\Tools\Powershell\pwsh.exe -ExecutionPolicy Bypass -NoExit -Command " & { C:\Tools\Config\windev\devel.ps1 -ToolsDir C:\Tools } "
+# C:\Tools\Powershell\pwsh.exe -ExecutionPolicy Bypass -NoExit -Command " & { C:\Tools\Config\windev\devel.ps1 -ToolsDir C:\Tools -StartupDir C:\ } "
 
 param(
-    [string]$ToolsDir
+    [string]$ToolsDir,
+	[string]$StartupDir
 )
 
 $thisDir = Split-Path -Parent $MyInvocation.MyCommand.Path
@@ -14,3 +15,5 @@ $env:TOOLS_DIR = "$ToolsDir"
 
 import-Module -Force $thisDir\windev.psd1 -DisableNameChecking
 Windev-InitializePath
+
+cd $StartupDir
